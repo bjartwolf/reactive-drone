5e5a4e736c20f035fe53a7511b6aa71344bb01a3
Bjartwolf <bjartwolf@bjartwolf-Dell-System-XPS-L321X.(none)>
2012-11-08 14:13:52 +0100

    cleaned up

--- timeFileReader.js
+++ timeFileReader.js
@@ -1,14 +1,21 @@
 // This thing reads a file line by line at a slow pace
 var fs = require('fs');
-//var byline = require('byline');
-var linestream = require('linestream');
-var stream = linestream.create('bebFile.txt');
-//var bebFileByLine = byline.createLineStream(fs.createReadStream('bebFile.txt'));
 var Stream = require('stream');
-var s = new Stream();
-s.writable = true;
-s.write = function(val) {console.log(val);setTimeout(function () {s.emit('drain');},1000);return false;};
-s.end = function(val) {console.log('end: ' + val);};
-//bebFileByLine.pipe(process.stdout);
-stream.pipe(s);
-//s.pipe(process.stdout);
+var linestream = require('linestream').create('bebFile.txt');
+
+// creating a slow linestream
+var timeInMsBetweenReadingLines = 1000;
+var slowStream = new Stream();
+slowStream.writable = true;
+slowStream.write = function(val) {
+    slowStream.emit('data', val);
+    setTimeout(function () {
+        slowStream.emit('drain');
+    } , timeInMsBetweenReadingLines);
+    return false; 
+};
+slowStream.end = function(val) { console.log('end of stream');};
+
+//connecting pipes
+linestream.pipe(slowStream);
+slowStream.pipe(process.stdout);
