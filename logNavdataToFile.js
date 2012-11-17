c47c9fb1600b6d60acaf7dfda3ca285a59d24192
Bjartwolf <bjartwolf@bjartwolf-Dell-System-XPS-L321X.(none)>
2012-11-17 21:50:36 +0100

    some changes in using bacon from npm

--- logNavdataToFile.js
+++ logNavdataToFile.js
@@ -1,38 +1,33 @@
-console.log('Press enter to stop logging');
-// To read from standard in it is set to resume
-process.stdin.resume();
-var zlib = require('zlib');
-var gzip = zlib.createGzip();
 var arDrone = require('ar-drone');
-var bacon = require('bacon.js').Bacon;
-var fs = require('fs');
+var gzip = require('zlib').createGzip();
+var bacon = require('baconjs').Bacon;
 var Stream = require('stream');
+var fs = require('fs');
 
-var client  = arDrone.createClient();
+var drone = arDrone.createClient();
 
-var s = new Stream();//No paranthesis in stream-handbook.. why?
-s.readable = true;
+// Lager en strøm av eventer fra navdata
+var eventStream = bacon.fromEventTarget(drone, 'navdata'); 
 
-var eventStream = bacon.fromEventTarget(client, 'navdata'); 
+// Lager en strøm for å skrive ut JSON-data og ny linje for
+// hvert navdata-event fra dronen
+var jsonStream = new Stream();
+jsonStream.readable = true;
 eventStream.onValue(function(x) {
-    s.emit('data', JSON.stringify(x) + "\n");
+    jsonStream.emit('data', JSON.stringify(x) + "\n");
 });
 
-// Creating a writeable filepipe and a zip-pipe
-var fileOut = fs.createWriteStream('output3.txt');
-var zipOut = fs.createWriteStream('zipout3.txt.gz');
+// Kobler alle strømmene sammen 
+jsonStream.pipe(gzip).pipe(fs.createWriteStream('logdata2.gz'));
 
-// Connecting pipes
-//s.pipe(process.stdout);
-s.pipe(fileOut);
-s.pipe(gzip).pipe(zipOut);
-
-// To end the stream when enter is pressed
-// It pauses for a second to make sure streams are closed, the zip-thingy 
-// breaks if quit to early. There has to be a better way...
+// Resten er bare for å kunne stoppe loggingen
+// Den tar en liten pause for å være sikker på at filen er lukket skikkelig
+// Det kan helt sikkert løses smartere
+console.log('Press enter to stop logging');
+// For å kunne lese fra stdin, som er pauset i node som default 
+process.stdin.resume();
 process.stdin.on('data', function () {
-    console.log('ending');
-    s.emit('end');
+    jsonStream.emit('end');
     setTimeout(function() {
         process.exit(1);}
     ,1000)
